49c8118d45335ed2aa68a10aa7d21a67cdc3b2cc
christiancostap <christiancosta.parreiras@yahoo.com.br>
2021-05-15 15:21:16 -0300

    Initial commit

---
+++ src/main/java/com/carregister/ZupActivity/ZupActivityApplication.java
@@ -0,0 +1,13 @@
+package com.carregister.ZupActivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupActivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carregister/ZupActivity/ZupActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carregister.ZupActivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupActivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
